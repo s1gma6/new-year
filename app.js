ad0ed6b10a4c54b1477b717f60f2fa98fe8c4448
Shivaram Kumar <78011687+s1gma6@users.noreply.github.com>
2021-12-31 09:06:20 +0530

    v.1.0.0
    More changes to come!

---
+++ app.js
@@ -0,0 +1,15 @@
+import { Fireworks } from 'fireworks-js'
+
+const container = document.querySelector('.fireworks-container')
+const fireworks = new Fireworks(container, {})
+fireworks.start()
+fireworks.pause()
+fireworks.clear()
+
+// stop and clear fireworks
+fireworks.stop()
+
+// after initialization you can change the fireworks parameters
+fireworks.setOptions({ delay: { min: 10, max: 15 } })
+
+
